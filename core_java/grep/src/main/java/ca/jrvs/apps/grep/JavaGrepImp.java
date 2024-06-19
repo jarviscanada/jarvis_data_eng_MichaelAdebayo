b70d38c73d91f09fa1243a1d165c86611d0d7a77
MichaelAdebayo <mikea14071@gmail.com>
2024-06-18 22:33:50 -0400

    Implement core functionality in JavaGrepImp.

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,108 +1,160 @@
 package ca.jrvs.apps.grep;
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class JavaGrepImp implements JavaGrep {
 
     final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
 
-    public static void main(String[] args) {
-        System.out.println("Just tetsing");
-    }
+    private String regex;
+    private String rootPath;
+    private String outFile;
 
-    /**
-     * @throws IOException
-     */
+
     @Override
     public void process() throws IOException {
+       List<String>matchedLines = new ArrayList<>();
+
+       for(File file : listFiles(rootPath)) {
+           for (String line : readLines(file)) {
+               if(containsPattern(line)){
+                   matchedLines.add(line);
+               }
+           }
+       }
+       writeToFile(matchedLines);
 
     }
 
-    /**
-     * @param rootDir input directory
-     * @return
-     */
     @Override
     public List<File> listFiles(String rootDir) {
-        return null;
+
+        List<File>matchedFiles = new ArrayList<>();
+        File dir = new File(rootDir);
+        File[] files = dir.listFiles();
+        for(File file : files) {
+            if(file.isDirectory()) {
+                matchedFiles.addAll(listFiles(file.getAbsolutePath()));
+            }
+            else {
+                matchedFiles.add(file);
+            }
+        }
+        return matchedFiles;
+
+
     }
 
-    /**
-     * @param inputFile file to be read
-     * @return
-     */
+
     @Override
     public List<String> readLines(File inputFile) {
-        return null;
+            List<String> lines = new ArrayList<>();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                logger.error("Error reading lines from file", e);
+            }
+            return lines;
+
     }
 
-    /**
-     * @param line input string
-     * @return
-     */
+
     @Override
     public boolean containsPattern(String line) {
-        return false;
+        return Pattern.compile(regex, Pattern.DOTALL).matcher(line).find();
+
     }
 
-    /**
-     * @param lines matched line
-     * @throws IOException
-     */
+
     @Override
     public void writeToFile(List<String> lines) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            logger.error("Error writing to file", e);;
+            throw e;
+        }
 
     }
 
-    /**
-     * @return
-     */
+
     @Override
     public String getRootPath() {
-        return "";
+
+        return rootPath;
     }
 
-    /**
-     * @param rootPath
-     */
     @Override
     public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
 
     }
 
-    /**
-     * @return
-     */
+
     @Override
     public String getRegex() {
-        return "";
+        return regex;
     }
 
-    /**
-     * @param regex
-     */
+
     @Override
     public void setRegex(String regex) {
+        this.regex = regex;
 
     }
 
-    /**
-     * @return
-     */
+
     @Override
     public String getOutFile() {
-        return "";
+        return outFile;
     }
 
-    /**
-     * @param outFile
-     */
+    
     @Override
     public void setOutFile(String outFile) {
 
+        this.outFile = outFile;
+    }
+
+
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new RuntimeException("Usage: JavaGrep regex rootPath outFile");
+        }
+        //Use default logger config
+        BasicConfigurator.configure();
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        }catch (Exception ex) {
+            javaGrepImp.logger.error("Error:Unable to process", ex);
+        }
     }
 }
